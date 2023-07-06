e871e4f027ee0c99ecb322dfdc68ed1ffe503c6c
Alex Cheema <alexcheema123@gmail.com>
2023-07-06 16:02:09 +0100

    download hez_15.ptau if it does not exist

--- circuits/generate.sh
+++ circuits/generate.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [ ! -f ./hez_15.ptau ]
+then
+	echo "hez_15.ptau not found. Downloading..."
+    curl https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau --output hez_15.ptau
+fi
+
 circom simplemm.circom --r1cs --wasm
 snarkjs plonk setup simplemm.r1cs hez_15.ptau simplemm_final.zkey            
 snarkjs zkey export verificationkey simplemm_final.zkey verification_key.json
