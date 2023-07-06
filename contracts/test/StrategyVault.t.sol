ced52212c06049406ec05ab7681fd0e13ef28aec
lucasege <lucasege@cs.stanford.edu>
2023-07-06 11:36:39 -0700

    wip

---
+++ contracts/test/StrategyVault.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+contract ReceiverAccountTest is Test {
+    function setUp() public {}
+
+    function test_execute() public {}
+}
