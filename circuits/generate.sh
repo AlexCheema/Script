0c51336bf9daf5833b248cc74eb92f5c78ce4381
Alex Cheema <alexcheema123@gmail.com>
2023-07-30 17:16:03 +0100

    support for other circuit names

--- circuits/generate.sh
+++ circuits/generate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CIRCUIT=${CIRCUIT:-simplemm}
+
 if [ ! -f ./hez_15.ptau ]
 then
 	echo "hez_15.ptau not found. Downloading..."
     curl https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau --output hez_15.ptau
 fi
 
-circom simplemm.circom --r1cs --wasm
-snarkjs plonk setup simplemm.r1cs hez_15.ptau simplemm_final.zkey            
-snarkjs zkey export verificationkey simplemm_final.zkey verification_key.json
-snarkjs zkey export solidityverifier simplemm_final.zkey ../contracts/Verifier.sol
+circom ${CIRCUIT}.circom --r1cs --wasm
+snarkjs plonk setup ${CIRCUIT}.r1cs hez_15.ptau ${CIRCUIT}_final.zkey
+snarkjs zkey export verificationkey ${CIRCUIT}_final.zkey verification_key.json
+snarkjs zkey export solidityverifier ${CIRCUIT}_final.zkey ../contracts/Verifier.sol
